a8badca35ba3289d025720d2278df1711b1384fc
Enes Aybars Aydın <aybarsaybars@gmail.com>
2022-08-01 16:16:06 +0300

    Feature C created

---
+++ src/featureC_Tasks/Store.java
@@ -0,0 +1,20 @@
+package featureC_Tasks;
+
+public class Store {
+
+    public static void main(String[] args) {
+
+        System.out.println("Store has been opened");
+
+        System.out.println("Hayırlı işler kardeş");
+
+
+
+
+
+
+
+
+
+    }
+}
